b962692a6eb7cd603d3c466ab1d64069e673433c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-05-24 19:35:09 -0600

    feat: description added to com ports

--- src/listserial/__init__.py
+++ src/listserial/__init__.py
@@ -3,19 +3,21 @@ import sys
 
 import serial
 
-# from: https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python?__cf_chl_tk=sRttuoRZRJoPKNFlEp3L6qAr_Y6WXl8KGTsQkzg4vdo-1748131557-1.0.1.1-CGa.p9qYeX3r45SujttJU20sGGU5PTK0X7gfwXiGMIs
+# inspired by https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python?__cf_chl_tk=sRttuoRZRJoPKNFlEp3L6qAr_Y6WXl8KGTsQkzg4vdo-1748131557-1.0.1.1-CGa.p9qYeX3r45SujttJU20sGGU5PTK0X7gfwXiGMIs
 
 
-def serial_ports() -> list[str]:
-    """Lists serial port names
+def serial_ports() -> list[tuple[str, str]]:
+    """Lists serial port names with descriptions
 
     :raises EnvironmentError:
         On unsupported or unknown platforms
     :returns:
-        A list of the serial ports available on the system
+        A list of tuples (port_name, description) of the serial ports available on the system
     """
     if sys.platform.startswith("win"):
-        ports = ["COM%s" % (i + 1) for i in range(256)]
+        from serial.tools import list_ports
+
+        return [(port.device, port.description) for port in list_ports.comports()]
     elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
         # this excludes your current terminal "/dev/tty"
         ports = glob.glob("/dev/tty[A-Za-z]*")
@@ -26,11 +28,12 @@ def serial_ports() -> list[str]:
 
     result = []
 
+    # For Linux/Mac we don't have built-in descriptions, so we'll just use the port name
     for port in ports:
         try:
             s = serial.Serial(port)
             s.close()
-            result.append(port)
+            result.append((port, ""))
         except (OSError, serial.SerialException):
             pass
     return result
@@ -44,5 +47,8 @@ def main() -> None:
     else:
         print("Serial ports:\n")
 
-        for p in ports:
-            print("\t" + "- " + p)
+        for port, description in ports:
+            port_info = f"- {port}"
+            if description:
+                port_info += f" ({description})"
+            print("\t" + port_info)
