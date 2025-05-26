d87e16267a766d48cbf92f7ceadae924ba264c42
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-05-26 12:58:35 -0600

    fix: imports were reorganized

--- src/listserial/__init__.py
+++ src/listserial/__init__.py
@@ -1,9 +1,9 @@
-import glob
+# inspired by
+# https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python?__cf_chl_tk=sRttuoRZRJoPKNFlEp3L6qAr_Y6WXl8KGTsQkzg4vdo-1748131557-1.0.1.1-CGa.p9qYeX3r45SujttJU20sGGU5PTK0X7gfwXiGMIs
+
 import sys
 
 import serial
-
-# inspired by https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python?__cf_chl_tk=sRttuoRZRJoPKNFlEp3L6qAr_Y6WXl8KGTsQkzg4vdo-1748131557-1.0.1.1-CGa.p9qYeX3r45SujttJU20sGGU5PTK0X7gfwXiGMIs
 
 
 def serial_ports() -> list[tuple[str, str]]:
@@ -14,14 +14,19 @@ def serial_ports() -> list[tuple[str, str]]:
     :returns:
         A list of tuples (port_name, description) of the serial ports available on the system
     """
+
     if sys.platform.startswith("win"):
         from serial.tools import list_ports
 
         return [(port.device, port.description) for port in list_ports.comports()]
     elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
+        import glob
+
         # this excludes your current terminal "/dev/tty"
         ports = glob.glob("/dev/tty[A-Za-z]*")
     elif sys.platform.startswith("darwin"):
+        import glob
+
         ports = glob.glob("/dev/tty.*")
     else:
         raise EnvironmentError("Unsupported platform")
