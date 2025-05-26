472cb68e21ca7f09db855f4ada6504e21529ae78
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-05-26 16:02:57 -0600

    refactor: removed os check
    uses `comports` to get port and description instead

--- src/listserial/__init__.py
+++ src/listserial/__init__.py
@@ -1,8 +1,3 @@
-import sys
-
-import serial
-
-
 def serial_ports() -> list[tuple[str, str]]:
     """Lists serial port names with descriptions
 
@@ -11,34 +6,9 @@ def serial_ports() -> list[tuple[str, str]]:
     :returns:
         A list of tuples (port_name, description) of the serial ports available on the system
     """
+    from serial.tools import list_ports
 
-    if sys.platform.startswith("win"):
-        from serial.tools import list_ports
-
-        return [(port.device, port.description) for port in list_ports.comports()]
-    elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
-        import glob
-
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob("/dev/tty[A-Za-z]*")
-    elif sys.platform.startswith("darwin"):
-        import glob
-
-        ports = glob.glob("/dev/tty.*")
-    else:
-        raise EnvironmentError("Unsupported platform")
-
-    result = []
-
-    # For Linux/Mac we don't have built-in descriptions, so we'll just use the port name
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append((port, ""))
-        except (OSError, serial.SerialException):
-            pass
-    return result
+    return [(port.device, port.description) for port in list_ports.comports()]
 
 
 def main() -> None:
